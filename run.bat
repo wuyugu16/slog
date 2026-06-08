d2d1244a2cf104e6d71df57de8fc675a65d6cbd3
wuyugu <wuyugu3624175@outlook.com>
2026-06-08 15:48:28 +0800

    ✅ test run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 git checkout only-safe
 git merge main --no-commit --no-ff
-D:\Git\bin\bash.exe -c "git reset -- docs/posts/*X* docs/.vuepress/**/*X* 2>/dev/null || true"
+git rm --cached -r --ignore-unmatch docs/posts/*X* docs/.vuepress/public/**/*X*
 git status
 git commit -m "merge main to only-safe"
 git checkout main
