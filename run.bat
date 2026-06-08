b5c4bffeb5c4bf4d86643c07865f1beceb693659
wuyugu <wuyugu3624175@outlook.com>
2026-06-08 15:54:56 +0800

    ✅ fix run.bat n+2

--- run.bat
+++ run.bat
@@ -3,4 +3,5 @@ git merge main --no-commit --no-ff
 git rm --cached -r --ignore-unmatch docs/posts/*X* docs/.vuepress/public/**/*X*
 git status
 git commit -m "merge main to only-safe"
-git checkout main
+git push safe-origin  
+git checkout main --force
