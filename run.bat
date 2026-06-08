1ac89a115d0f5906cdba75dc139e086106eeaa9b
wuyugu <wuyugu3624175@outlook.com>
2026-06-08 11:17:16 +0800

    ✅ fix bash

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 git checkout only-safe
 git merge main --no-commit --no-ff
-bash -c "git reset -- docs/posts/*X* docs/.vuepress/**/*X* 2>/dev/null || true"
+D:\Git\bin\bash.exe -c "git reset -- docs/posts/*X* docs/.vuepress/**/*X* 2>/dev/null || true"
 git status
 git commit -m "merge main to only-safe"
 git checkout main
